fe634a4bd82329c69292e6ac02457d87f4796ca6
Andrew <aj.reitz@gmail.com>
2020-04-13 12:01:07 -0500

    Added Ability to Test Crashlytics

--- src/debug/java/cash/andrew/lightalarm/TestActivity.kt
+++ src/debug/java/cash/andrew/lightalarm/TestActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import cash.andrew.lightalarm.data.Alarm
 import cash.andrew.lightalarm.databinding.ActivityNotificationTestBinding
 import java.time.LocalTime
+import kotlin.time.ExperimentalTime
+import kotlin.time.seconds
 
-class NotificationTestActivity: Activity() {
+class TestActivity: Activity() {
 
     private lateinit var binding: ActivityNotificationTestBinding
 
+    @ExperimentalTime
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,10 +22,14 @@ class NotificationTestActivity: Activity() {
         val notificationManager = alarmAppComponent.lightServiceComponent
             .notificationManager
 
-        binding.button.setOnClickListener {
+        binding.testAlarmNotification.setOnClickListener {
             it.postDelayed({
                 notificationManager.showAlarmNotification(Alarm(time = LocalTime.now()))
-            }, 3000)
+            }, 3.seconds.toLongMilliseconds())
+        }
+
+        binding.testCrash.setOnClickListener {
+            throw Exception("☠️ Test Crash ️☠️")
         }
     }
 }
