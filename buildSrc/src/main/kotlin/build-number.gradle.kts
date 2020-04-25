2a9c5a0cc8739165ca18f3f45fdd83d947ec5b8b
Andrew <aj.reitz@gmail.com>
2020-04-25 10:47:32 -0500

    Fixed Build Issue

--- buildSrc/src/main/kotlin/build-number.gradle.kts
+++ buildSrc/src/main/kotlin/build-number.gradle.kts
@@ -1,4 +1,4 @@
 extra["buildNumber"] = when {
-    hasProperty("cash.andrew.mntrail.buildNumber") -> properties["cash.andrew.mntrail.buildNumber"]
+    hasProperty("cash.andrew.lightalarm.buildNumber") -> properties["cash.andrew.lightalarm.buildNumber"]
     else -> System.getenv("LIGHT_ALARM_BUILD_NUMBER") ?: ""
 }
