df31e303034b4635a85300b7ad4de8fa867a1d33
Andrew Reitz <andrew.reitz@instacart.com>
2022-10-07 14:32:00 -0500

    Update to 33 and Libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    val kotlinVersion = "1.7.20"
-    id("com.android.application") version "7.2.0"
-    id("kotlin-android") version kotlinVersion
-    id("kotlin-kapt") version kotlinVersion
-    id("com.google.gms.google-services") version "4.3.14"
-    id("com.google.firebase.crashlytics") version "2.9.2"
-    id("com.github.ben-manes.versions") version "0.42.0"
-    id("com.github.triplet.play") version "3.7.0"
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+
+    alias(libs.plugins.google.services)
+    alias(libs.plugins.firebase.crashlytics)
+    alias(libs.plugins.play.publisher)
+
     id("build-number")
     id("android-signing-config")
 }
@@ -19,24 +19,18 @@ play {
     defaultToAppBundles.set(true)
 }
 
-repositories {
-    google()
-    mavenCentral()
-    jcenter()
-}
-
 android {
-    compileSdkVersion(33)
+    compileSdk = 33
     buildToolsVersion = "30.0.3"
 
     defaultConfig {
         applicationId = "cash.andrew.lightalarm"
-        minSdkVersion(26)
-        targetSdkVersion(33)
+        minSdk = 26
+        targetSdk = 33
 
         val buildNumber: String by project
         versionCode = if (buildNumber.isBlank()) 1 else buildNumber.toInt()
-        versionName = "三"
+        versionName = "yon"
     }
 
     signingConfigs {
@@ -85,32 +79,44 @@ android {
     viewBinding {
         isEnabled = true
     }
+
+    buildFeatures {
+        viewBinding = true
+        aidl = false
+        buildConfig = true
+        compose = false
+        prefab = false
+        renderScript = false
+        resValues = false
+        shaders = false
+    }
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib"))
 
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.dynamicanimation:dynamicanimation:1.0.0")
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.dynamicanimation)
 
-    implementation("com.google.firebase:firebase-analytics:21.1.1")
-    implementation("com.google.firebase:firebase-crashlytics:18.2.13")
+    implementation(platform("com.google.firebase:firebase-bom:${libs.versions.firebase.get()}"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-crashlytics-ktx")
 
-    implementation("com.google.android.material:material:1.6.1")
-    implementation("com.google.dagger:dagger:2.44")
-    kapt("com.google.dagger:dagger-compiler:2.44")
+    implementation(libs.material)
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
 
-    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation(libs.timber)
 
-    implementation("io.paperdb:paperdb:2.7.1")
+    implementation(libs.paper)
 
-    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
-    testImplementation("com.google.truth:truth:1.1.3")
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.truth)
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
