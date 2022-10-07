680bee6b8231eb31e6821b329b81a4aecc73b4cc
Andrew Reitz <andrew.reitz@instacart.com>
2022-10-07 13:48:07 -0500

    Updated Library Versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,22 @@
 plugins {
-    val kotlinVersion = "1.3.71"
-    id("com.android.application") version "3.6.2"
+    val kotlinVersion = "1.7.20"
+    id("com.android.application") version "7.2.0"
     id("kotlin-android") version kotlinVersion
     id("kotlin-kapt") version kotlinVersion
-    id("com.google.gms.google-services") version "4.3.3"
-    id("com.google.firebase.crashlytics") version "2.0.0"
-    id("com.github.ben-manes.versions") version "0.28.0"
-    id("com.github.triplet.play") version "2.7.5"
+    id("com.google.gms.google-services") version "4.3.14"
+    id("com.google.firebase.crashlytics") version "2.9.2"
+    id("com.github.ben-manes.versions") version "0.42.0"
+    id("com.github.triplet.play") version "3.7.0"
     id("build-number")
     id("android-signing-config")
 }
 
 play {
-    serviceAccountCredentials = file(properties["cash.andrew.lightalarm.publishKey"] ?: "keys/publish-key.json")
-    track = "internal"
+    serviceAccountCredentials.set(
+        rootProject.file(properties["cash.andrew.lightalarm.publishKey"] ?: "keys/publish-key.json")
+    )
+    track.set("internal")
+    defaultToAppBundles.set(true)
 }
 
 repositories {
@@ -23,13 +26,13 @@ repositories {
 }
 
 android {
-    compileSdkVersion(29)
-    buildToolsVersion("29.0.3")
+    compileSdkVersion(33)
+    buildToolsVersion = "30.0.3"
 
     defaultConfig {
         applicationId = "cash.andrew.lightalarm"
         minSdkVersion(26)
-        targetSdkVersion(29)
+        targetSdkVersion(33)
 
         val buildNumber: String by project
         versionCode = if (buildNumber.isBlank()) 1 else buildNumber.toInt()
@@ -87,31 +90,29 @@ android {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.core:core-ktx:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.0-beta4")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.dynamicanimation:dynamicanimation:1.0.0")
 
-    implementation("com.google.firebase:firebase-analytics:17.4.0")
-    implementation("com.google.firebase:firebase-crashlytics:17.0.0")
+    implementation("com.google.firebase:firebase-analytics:21.1.1")
+    implementation("com.google.firebase:firebase-crashlytics:18.2.13")
 
-    implementation("com.google.android.material:material:1.1.0")
-    implementation("com.google.dagger:dagger:2.27")
-    kapt("com.google.dagger:dagger-compiler:2.27")
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("com.google.dagger:dagger:2.44")
+    kapt("com.google.dagger:dagger-compiler:2.44")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
-    implementation("com.jakewharton.timber:timber:4.7.1")
+    implementation("com.jakewharton.timber:timber:5.0.1")
 
-    implementation("io.paperdb:paperdb:2.6")
+    implementation("io.paperdb:paperdb:2.7.1")
 
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
-    testImplementation("com.google.truth:truth:1.0.1")
+    testImplementation("com.google.truth:truth:1.1.3")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    targetCompatibility = "1.8"
-    sourceCompatibility = "1.8"
     kotlinOptions.jvmTarget = "1.8"
 }
